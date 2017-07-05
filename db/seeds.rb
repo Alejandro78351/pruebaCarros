e15de486ed0ded8ddee4a1f0167965dabdfdecab
jasson <ja7rodal@gmail.com>
2017-07-05 16:12:04 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Carro.create(latit: "321321", longit: "65423")
+Carro.create(latit: "4234231", longit: "654276573")
+Carro.create(latit: "756765", longit: "65765423")
